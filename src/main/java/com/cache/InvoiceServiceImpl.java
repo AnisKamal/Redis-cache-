0f92ff2ef72cf47375623d3f86c6b33e9d889c9e
AnisKamal <aniskamal52@gmail.com>
2022-10-12 11:31:46 +0100

    commit code

---
+++ src/main/java/com/cache/InvoiceServiceImpl.java
@@ -0,0 +1,55 @@
+package com.cache;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class InvoiceServiceImpl implements InvoiceService{
+
+    @Autowired
+    private InvoiceRepository invoiceRepo ;
+
+    @Override
+    public Invoice saveInvoice(Invoice inv) {
+        return invoiceRepo.save(inv);
+    }
+
+    @Override
+    @CachePut(value = "invoice", key="#invId")
+    public Invoice updateInvoice(Invoice inv, Integer invId) {
+
+        Invoice invoice = invoiceRepo.findById(invId)
+                .orElseThrow(() -> new InvoiceNotFoundException("Invoice Not Found"));
+        invoice.setInvAmount(inv.getInvAmount());
+        invoice.setInvName(inv.getInvName());
+        return invoiceRepo.save(invoice);
+    }
+
+    @Override
+    @CacheEvict(value = "Invoice", key="#invId")
+    public void deleteInvoice(Integer invId) {
+        Invoice invoice = invoiceRepo.findById(invId)
+                .orElseThrow(() -> new InvoiceNotFoundException("Invoice Not Found"));
+        invoiceRepo.delete(invoice);
+    }
+
+    @Override
+    @Cacheable(value = "Invoice", key="#invId")
+    public Invoice getOneInvoice(Integer invId) {
+        Invoice invoice = invoiceRepo.findById(invId)
+                .orElseThrow(() -> new InvoiceNotFoundException("Invoice Not Found"));
+
+        return invoice;
+    }
+
+    @Override
+    @Cacheable
+    public List<Invoice> getAllInvoices() {
+        return invoiceRepo.findAll();
+    }
+}
